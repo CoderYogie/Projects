2e19c8cc7aa1f40d58398c3fbb8cce568ef1b022
Yogesh Ingale <yingale1@binghamton.edu>
2022-02-05 21:59:08 -0500

    Commit 2

--- main/prj2-sol/src/params.mjs
+++
@@ -1,9 +0,0 @@
-/** Application parameters used across application */
-export default {
-  /** default count of results when returning multiple results */
-  defaultCount: 5,
-
-  /** location of Bing EB */
-  bingLoc: { lat: 42.087225457002376, lng: -75.96795097953378 },
-
-};
